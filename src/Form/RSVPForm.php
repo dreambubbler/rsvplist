bc9a396b7adcf7343cf193a8b9807344de121f89
Dallas Ramsden <engineering@informlearning.com>
2020-08-29 21:43:28 +1200

    Use for your learning.

--- src/Form/RSVPForm.php
+++ src/Form/RSVPForm.php
@@ -72,8 +72,51 @@ class RSVPForm extends FormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, \Drupal\Core\Form\FormStateInterface $form_state) {
+    // Use this code for video 03_04_Building an RSVP Form
+    //    $email = $form_state->getValue('email');
+    //    $this->messenger()->addMessage(t("The form is working. The user entered @entry.",
+    //                                  ['@entry' => $email]));
+
+    // Use below code for videos 03_08 onward.
+
+    // Get current user ID.
+    $uid = \Drupal::currentUser()->id();
+
+    // Demonstration for how to load a full user object of the current user. This variable is not needed for this code,
+    // but is shown for demonstration purposes.
+    $user = \Drupal\user\Entity\User::load($uid);
+
+    // Establish the required variables to eventually insert into the database.
     $email = $form_state->getValue('email');
-    $this->messenger()->addMessage(t("The form is working. The user entered @entry.",
-                                  ['@entry' => $email]));
+    $nid = $form_state->getValue('nid');
+    $current_time = time();
+
+
+    // Begin to build a query builder object $query.
+    // https://www.drupal.org/docs/8/api/database-api/insert-queries
+    $query = \Drupal::database()->insert('rsvplist');
+
+    // Specify the fields that the query will insert into.
+    $query->fields([
+      'mail',
+      'nid',
+      'uid',
+      'created',
+    ]);
+
+    // Set the values of the fields we selected. Note that they must be in the same order as we defined them
+    // in the $query->fields([...]) above.
+    $query->values([
+      $email,
+      $nid,
+      $uid,
+      $current_time,
+    ]);
+
+    // Execute the query! Drupal will handle the correct syntax of the query automatically!
+    $query->execute();
+
+    // Provide the form submitter a nice message.
+    $this->t('Thank you for your RSVP, you are on the list for the event!');
   }
 }
