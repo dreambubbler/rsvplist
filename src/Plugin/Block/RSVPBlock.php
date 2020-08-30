5ede79638db071fa5ceba9c49fed65d77adb467d
Dallas Ramsden <engineering@informlearning.com>
2020-08-30 21:09:57 +1200

    Use for your learning.

--- src/Plugin/Block/RSVPBlock.php
+++ src/Plugin/Block/RSVPBlock.php
@@ -17,6 +17,7 @@ use Drupal\Core\Block\BlockBase;
  * @Block(
  *   id = "rsvp_block",
  *   admin_label = @Translation("RSVP Block")
+ *   category = @Translation("RSVP")
  * )
  */
 class RSVPBlock extends BlockBase {
