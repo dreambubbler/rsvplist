11f9eb1a1632c529e875bf33163d0f9fd484315a
Dallas Ramsden <engineering@informlearning.com>
2020-08-30 21:37:17 +1200

    Use for your learning.

--- src/Plugin/Block/RSVPBlock.php
+++ src/Plugin/Block/RSVPBlock.php
@@ -7,9 +7,12 @@
 
 namespace Drupal\rsvplist\Plugin\Block;
 
-
 use Drupal\Core\Block\BlockBase;
 
+// Use code below for exercise 03_11
+use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\Access\AccessResult;
+// end 03_11
 
 /**
  * Provides the RSVP main block.
@@ -24,13 +27,34 @@ class RSVPBlock extends BlockBase {
   /**
    * {@inheritdoc}
    */
-  public function build()
-  {
+  public function build() {
     // Use this code for exercise 03_10_Adding a Block
-    return [
+    /*return [
       '#type' => 'markup',
       '#markup' => $this->t('My RSVP List Block'),
-    ];
-    // end 03_10 03_10_Adding a Block
+    ];*/
+    // end 03_10_Adding a Block
+
+    // Use this code for exercise 03_11 Setting Block Permissions
+    return \Drupal::formBuilder()->getForm('Drupal\rsvplist\Form\RSVPForm');
+    // end 03_11
+
   }
+
+  // Add this method for exercise 03_11
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockAccess(AccountInterface $account) {
+    $node = \Drupal::routeMatch()->getParameter('node');
+    $nid = $node->nid->value;
+    if (is_numeric($nid)) {
+      return AccessResult::allowedIfHasPermission($account, 'view rsvplist');
+    }
+    else {
+      return AccessResult::forbidden();
+    }
+  }
+
 }
