853904acf359a1d3047b218e02a91889f7b298b2
Dallas Ramsden <engineering@informlearning.com>
2020-08-30 21:00:29 +1200

    Use for your learning.

---
+++ src/Plugin/Block/RSVPBlock.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * @file
+ * Creates a block which displays the the RSVPForm contained in RSVPForm.php
+ */
+
+namespace Drupal\rsvplist\Plugin\Block;
+
+
+use Drupal\Core\Block\BlockBase;
+
+
+/**
+ * @Block(
+ *   id = "rsvp_block",
+ *   admin_label = @Translation("RSVP Block")
+ * )
+ */
+class RSVPBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build()
+  {
+    return [
+      '#type' => 'markup',
+      '#markup' => $this->t('My RSVP List Block'),
+    ];
+  }
+}
