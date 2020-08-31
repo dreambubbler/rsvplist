344dd0743b8663dfe5cc8162a6c16c4ea2806b30
Dallas Ramsden <engineering@informlearning.com>
2020-08-31 22:31:05 +1200

    Use for your learning.

---
+++ src/EnablerService.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+ * @file
+ * Contains the RSVP Enabler service.
+ */
+
+namespace Drupal\rsvplist;
+
+
+use Drupal\Core\Database\Database;
+use Drupal\jsonapi\JsonApiResource\Data;
+use Drupal\node\Entity\Node;
+
+class EnablerService {
+
+  public function __construct() {
+
+  }
+
+  /**
+   * Sets an individual node to be RSVP enabled.
+   *
+   * @param Node $node
+   * @throws \Exception
+   */
+  public function setEnabled(Node $node) {
+    try {
+      if (!this->isEnabled($node)) {
+        $insert = Database::getConnection()->insert('rsvplist_enabled');
+        $insert->fields(['nid'], [$node->id()]);
+        $insert->execute();
+      }
+    }
+    catch (\Exception $e) {
+      \Drupal::messenger()->addError(t('Unable to save RSVP settings at this time due to database error. Please try again.'));
+    }
+  }
+
+  /**
+   * Checks if an individual node is RSVP enabled.
+   *
+   * @param Node $node
+   * @return bool
+   *  whether or not the node is enabled for the RSVP functionality.
+   */
+  public function isEnabled(Node $node) {
+    if ($node->isNew()) {
+      return FALSE;
+    }
+    $select = Database::getConnection()->select('rsvplist_enabled', 're');
+    $select->fields('re', ['nid']);
+    $select->condition('nid', $node->id());
+    $results = $select->execute();
+
+    return !(empty($results->fetchCol()));
+  }
+
+  /**
+   * Deletes enabled settings for an individual node.
+   *
+   * @param Node $node
+   */
+  public function delEnabled(Node $node) {
+    $delete = Database::getConnection()->delete('rsvplist_enabled');
+    $delete->condition('nid', $node->id());
+    $delete->execute();
+  }
+}
