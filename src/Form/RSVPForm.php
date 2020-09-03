216ed1318d86bd5950edccc173dcacdaf58e2926
Dallas Ramsden <engineering@informlearning.com>
2020-09-03 23:14:10 +1200

    Use for your learning.

--- src/Form/RSVPForm.php
+++ src/Form/RSVPForm.php
@@ -74,9 +74,9 @@ class RSVPForm extends FormBase {
    */
   public function submitForm(array &$form, \Drupal\Core\Form\FormStateInterface $form_state) {
     // < Use this code for video 03_02_01_Part1/2-Build the Email Submission Form for RSVP List
-    //    $email = $form_state->getValue('email');
+    //    $submitted_email = $form_state->getValue('email');
     //    $this->messenger()->addMessage(t("The form is working. You entered @entry.",
-    //                                  ['@entry' => $email]));
+    //                                  ['@entry' => $submitted_email]));
     // End 03_02_01_Part1/2-Build the Email Submission Form for RSVP List />
 
 
