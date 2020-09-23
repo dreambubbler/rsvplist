930039dc9bd41030a45948951c41f76df5da1059
Dallas Ramsden <engineering@informlearning.com>
2020-09-23 16:39:04 +1200

    Use for your learning.

--- src/Controller/ReportController.php
+++ src/Controller/ReportController.php
@@ -2,7 +2,8 @@
 
 /**
  * @file
- * Provide site administrators with a list of all the RSVP List signups so they know who is attending their events.
+ * Provide site administrators with a list of all the RSVP List signups
+ * so they know who is attending their events.
  */
 
 namespace Drupal\rsvplist\Controller;
@@ -12,49 +13,65 @@ use Drupal\Core\Database\Database;
 
 class ReportController extends ControllerBase {
 
+  /**
+   * Gets and returns all RSVPs for all nodes.
+   * These are returned as an associative array, with each row
+   * containing the username, the node title, and email of RSVP.
+   *
+   * @return array|null
+   */
   protected function load() {
     try {
 
-      // https://www.drupal.org/docs/8/api/database-api/dynamic-queries/introduction-to-dynamic-queries
+      // https://www.drupal.org/docs/8/api/database-api/dynamic-
+      //         queries/introduction-to-dynamic-queries
       $database = \Drupal::database();
-      $select = $database->select('rsvplist', 'r');
+      $select_query = $database->select('rsvplist', 'r');
 
       // Join the user table, so we can get the entry creator's username.
-      $select->join('users_field_data', 'u', 'r.uid = u.uid');
+      $select_query->join('users_field_data', 'u', 'r.uid = u.uid');
 
       // Join the node table, so we can get the event's name.
-      $select->join('node_field_data', 'n', 'r.nid = n.nid');
+      $select_query->join('node_field_data', 'n', 'r.nid = n.nid');
 
       // Select these specific fields for the output.
-      $select->addField('u', 'name', 'username');
-      $select->addField('n', 'title');
-      $select->addField('r', 'mail');
+      $select_query->addField('u', 'name', 'username');
+      $select_query->addField('n', 'title');
+      $select_query->addField('r', 'mail');
 
-      // Note that fetchAll() and fetchAllAssoc() will by default fetch using whatever fetch mode was set
-      // on the query (numeric array, associative array, or object).
-      // That can be modified by passing in a new fetch mode constant. For fetchAll(), it is the first parameter.
+      // Note that fetchAll() and fetchAllAssoc() will, by default, fetch using
+      // whatever fetch mode was set on the query
+      // (i.e. numeric array, associative array, or object).
+      // Fetches can be modified by passing in a new fetch mode constant.
+      // For fetchAll(), it is the first parameter.
       // https://www.drupal.org/docs/8/api/database-api/result-sets
       // https://www.php.net/manual/en/pdostatement.fetch.php
-      $entries = $select->execute()->fetchAll(\PDO::FETCH_ASSOC);
+      $entries = $select_query->execute()->fetchAll(\PDO::FETCH_ASSOC);
 
       // Return the associative array of RSVPList entries.
       return $entries;
     }
     catch (\Exception $e) {
       // Display a user-friendly error.
-      \Drupal::messenger()->addStatus(t('Unable to access the database at this time. Please try again later.'));
+      \Drupal::messenger()->addStatus(
+        t('Unable to access the database at this time. Please try again later.')
+      );
       return NULL;
     }
   }
 
+  /**
+   * Creates the RSVPList report page.
+   *
+   * @return array
+   *  Render array for the RSVPList report output.
+   */
   public function report() {
     $content = [];
 
-    // https://api.drupal.org/api/drupal/core!lib!Drupal!Core!Render!theme.api.php/group/theme_render/9.0.x
-
     $content['message'] = [
-      '#markup' => t('Below is a list of all Event RSVPs including username, email address
-                             and the name of the event they will be attending.'),
+      '#markup' => t('Below is a list of all Event RSVPs including username,
+              email address and the name of the event they will be attending.'),
     ];
 
     $headers = [
@@ -63,21 +80,30 @@ class ReportController extends ControllerBase {
       t('Email'),
     ];
 
-    $rows = [];
-    // Load the entries using the above load() method.
-    $entries = $this->load();
+    // Because load() returns an associative array with each table row
+    // as its own array, we can simply define the HTML table rows like this:
+    $table_rows = $this->load();
 
-    // Go through each entry and add it to $rows. Ultimately each array will be rendered as a row in
-    // an HTML table.
-    foreach ($entries as $entry) {
-      $rows[] = $entry;
-    }
+    // However, as an example, if load() did not return the results in
+    // a structure compatible with what we need, we could populate the
+    // $table_rows variable like so:
+    /**
+      $table_rows = [];
+      // Load the entries from database.
+      $rsvp_entries = $this->load();
+
+      // Go through each entry and add it to $rows.
+      // Ultimately each array will be rendered as a row in an HTML table.
+      foreach ($rsvp_entries as $entry) {
+        $table_rows[] = $entry;
+      }
+     */
 
     // Create the render array for rendering an HTML table.
     $content['table'] = [
       '#type' => 'table',
       '#header' => $headers,
-      '#rows' => $rows,
+      '#rows' => $table_rows,
       '#empty' => t('No entries available.'),
     ];
 
